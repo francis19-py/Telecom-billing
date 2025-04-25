4e03ce86c400a5f0b8e543f873e0ca9ed5c43890
francis19-py <francisgithambo80@gmail.com>
2025-04-25 15:23:45 +0300

    small edit

--- viewing.c
+++ viewing.c
@@ -2,7 +2,7 @@
 void viewRecords () {
     if (customerCount == 0){
         printf("\n No records to display.\n");
-        return 1;
+        return ;
     printf("\n Name\tPhone Number\tUsage(min)\tTotal Bill($)\n");
     for (int i = 0; i < customerCount; i++){
         printf("%s\t%s\t%.2f\t\t%.2f\n", customers[i].name, customers[i].phoneNumber, customers[i].usage, customers[i].TotalBill);
