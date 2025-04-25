59dfb66a0a53d0219e5b18d58d9da94ea5511ecd
francis19-py <francisgithambo80@gmail.com>
2025-04-25 14:54:55 +0300

    corrections

--- telecombiling.c
+++ telecombiling.c
@@ -68,7 +68,7 @@ void modifyRecord(const char *phoneNumber)
 {
     for (int i = 0; i < customerCount; i++)
     {
-        if (strcmp(customer[i].phoneNumber, phoneNumber) == 0)
+        if (strcmp(customers[i].phoneNumber, phoneNumber) == 0)
         {       
         printf("\nEnter new usage (in minutes) for %s:", customers[i].name);
         if (scanf("%f", &customers[i].usage)!= 1 ||customers[i].usage<0){
@@ -92,7 +92,7 @@ void viewRecords () {
         return 1;
     printf("\n Name\tPhone Number\tUsage(min)\tTotal Bill($)\n");
     for (int i = 0; i < customerCount; i++){
-        printf("%s\t%s\t%.2f\t\t%.2f\n", customers[i].name, customers[i].phoneNumber, customers[i].usage, customers[i].TotalBill);
+        printf("%s\t%s\t%.2f\t\t%.2f\n", customers[i].name, customers[i].phoneNumber, customers[i].usage, customers[i].totalBill);
     }
 }
 }
