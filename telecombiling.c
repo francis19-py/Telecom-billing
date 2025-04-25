4ba57ab96ed0f55ef27bbabcd00173b2e6499762
francis19-py <francisgithambo80@gmail.com>
2025-04-25 15:40:00 +0300

    correcting error in view record function

--- telecombiling.c
+++ telecombiling.c
@@ -90,11 +90,11 @@ void viewRecords () {
     if (customerCount == 0){
         printf("\n No records to display.\n");
         return;
+    }
     printf("\n Name\tPhone Number\tUsage(min)\tTotal Bill($)\n");
     for (int i = 0; i < customerCount; i++){
         printf("%s\t%s\t%.2f\t\t%.2f\n", customers[i].name, customers[i].phoneNumber, customers[i].usage, customers[i].totalBill);
     }
-}
 }
 
 //function to delete records
