4e72ad6b1c81361b3fa0d4aa45d98d39ea3adc0e
Denis Omondi <153709950+denis-omondi@users.noreply.github.com>
2025-04-25 13:18:31 +0300

    ViewPayment
    viewpayment

--- Structure.c
+++ Structure.c
@@ -124,3 +124,15 @@ void searchRecord(char phoneNumber[]) {
 	printf("\nRecord not found!\n"); 
 }
  
+//Function to viewpayment for a customer
+void viewpayment (const char *phoneNumber)
+{
+    for (int i =0; i < customerCount; i++){
+        if (strcmp(customers[i].phoneNumber, phoneNumber)==0){
+            printf("\nTotal Bill for %s: $%.2f\n", customers[i].name, customers[i].totalBill);
+            return;
+        }
+    }
+     printf("\nRecord not found!\n");
+}
+
