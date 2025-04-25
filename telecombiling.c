2dfeb10d40611d56808149958d4d4ecec5aa2b8d
francis19-py <francisgithambo80@gmail.com>
2025-04-25 15:19:56 +0300

    changes

--- telecombiling.c
+++ telecombiling.c
@@ -89,7 +89,7 @@ void modifyRecord(const char *phoneNumber)
 void viewRecords () {
     if (customerCount == 0){
         printf("\n No records to display.\n");
-        return 1;
+        return;
     printf("\n Name\tPhone Number\tUsage(min)\tTotal Bill($)\n");
     for (int i = 0; i < customerCount; i++){
         printf("%s\t%s\t%.2f\t\t%.2f\n", customers[i].name, customers[i].phoneNumber, customers[i].usage, customers[i].totalBill);
@@ -178,7 +178,7 @@ int main (){
                 printf("\nEnter phone number to view payment: ");
                 scanf("%14s", phoneNumber);
                 clearInputBuffer();
-                viewpayent(phoneNumber);
+                viewpayment(phoneNumber);
                 break;
         case 5:
                 printf("\nEnter phone number to delete record:  ");
